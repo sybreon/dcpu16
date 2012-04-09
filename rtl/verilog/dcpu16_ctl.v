b8e2bc19ae86a7b2d41d29434d4e9f0f37d7f634
Shawn Tan <shawn.tan@aeste.my>
2012-04-10 01:39:59 +0800

    Add missing license.

--- rtl/verilog/dcpu16_ctl.v
+++ rtl/verilog/dcpu16_ctl.v
@@ -1,3 +1,22 @@
+/*
+ DCPU16 Verilog Implementation
+ Copyright (C) 2012 Shawn Tan <shawn.tan@sybreon.com>
+ 
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU Lesser General Public License as
+ published by the Free Software Foundation, either version 3 of the
+ License, or (at your option) any later version.  This program is
+ distributed in the hope that it will be useful, but WITHOUT ANY
+ WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ for more details.
+ 
+ You should have received a copy of the GNU Lesser General Public
+ License along with this program.  If not, see
+ <http://www.gnu.org/licenses/>.  */
+
+// TODO: JSR
+// FIXME: Reg-Reg forwarding
 
 module dcpu16_ctl (/*AUTOARG*/
    // Outputs
